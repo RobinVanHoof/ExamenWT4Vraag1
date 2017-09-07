ff35844a9ef6874deb1664afe704e9471b3f755f
RobVH92 <robin_van_hoof@hotmail.com>
2017-09-07 15:33:34 +0200

    add get from url

---
+++ src/moviespackage/JedisConnection.java
@@ -0,0 +1,26 @@
+package moviespackage;
+
+import redis.clients.jedis.Jedis;
+
+// Een singleton past erg goed bij een db connectie
+public class JedisConnection {
+
+	private static final String HOST = "localhost";
+	private static JedisConnection instance;
+	private Jedis connection;
+
+	public static JedisConnection getInstance() {
+		if (instance == null) {
+			instance = new JedisConnection();
+		}
+		return instance;
+	}
+
+	private JedisConnection() {
+		this.connection = new Jedis(HOST);
+	}
+
+	public Jedis getConnection() {
+		return this.connection;
+	}
+}
---
+++ src/moviespackage/MovieResource.java
@@ -0,0 +1,102 @@
+package moviespackage;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.Cursor;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+import moviespackage.JedisConnection;
+import jdk.nashorn.internal.parser.JSONParser;
+import redis.clients.jedis.Jedis;
+
+@Path("/movies")
+public class MovieResource {
+	private int counter = 0;
+	@GET
+	@Path("{title}")
+	@Produces({"text/html"})
+	public String searchMovie(@PathParam("title") String title){
+		
+		Jedis jedis = JedisConnection.getInstance().getConnection();
+		
+		String returnJSON = "";
+		
+		Boolean found = false;
+		
+
+		for(String titlee : jedis.keys("Title:*")){
+			String tmpTitle = jedis.get(titlee);
+			if(tmpTitle.equalsIgnoreCase(title)){
+				found = true;
+			}
+		}
+
+		if (!found){
+			
+			Response response = ClientBuilder.newClient()
+					.target("http://www.omdbapi.com/?t=" + title + "&apikey=plzBanMe")
+					.request(MediaType.APPLICATION_JSON)
+					.get();
+			
+			String jsonString = response.readEntity(String.class);
+			
+			JsonReader jsonReader = Json.createReader(new StringReader(jsonString));
+			JsonObject object = jsonReader.readObject();
+			jsonReader.close();
+			counter = counter + 1;
+			System.out.println(counter);
+			jedis.set("Title:" + counter, "Deadpool");
+			jedis.set("Year:" + counter, object.getString("Year"));
+			jedis.set("Actors:" + counter, object.getString("Actors"));
+			
+			JsonObjectBuilder builder = Json.createObjectBuilder();
+	        builder.add("Year", object.getString("Year"));
+	        builder.add("Actors", object.getString("Actors"));
+	        JsonObject newJSON = builder.build();
+	        
+	        System.out.println("toegevoegd aan db");
+			
+			returnJSON = newJSON.toString();
+		}
+		else{
+			JsonObjectBuilder builder = Json.createObjectBuilder();
+			for (String title2 : jedis.keys("Title:*")) {
+				String tmpTitle = jedis.get(title2);
+				if (tmpTitle.equalsIgnoreCase(title)) {
+					int titleId = Integer.parseInt(title2.split(":")[1]);
+					
+					builder.add("Year", jedis.get("Year:" + titleId));
+					builder.add("Actors", jedis.get("Actors:" + titleId));
+					
+					JsonObject newJSON = builder.build();
+					System.out.println("al eerder opgeslagen");
+					
+					returnJSON = newJSON.toString();
+				}
+			}
+		}
+		return returnJSON;
+	}
+}
